4ab0bde50cfd4a986ca7c3cca387e62896dfc3f9
Inaam2 <inaam.mohammed29@gmail.com>
2023-09-17 17:46:45 -0400

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Upload Image of Animal Here</h1>
+        <h1>TAILTRACKr</h1>
 
         {this.state.selectedImage && (
           <div>

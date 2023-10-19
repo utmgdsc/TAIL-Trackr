a427010da14d36c559eb782637eb9693b59b3e3f
MD-Hamza <71731160+MD-Hamza@users.noreply.github.com>
2023-10-19 15:17:22 -0400

    Attached MongoDB server

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,10 @@
 from flask import Flask, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('127.0.0.1', 27017)
+db = client['Tail-TrackR']
+collection = db['User']
 
 @app.route("/api/upload", methods=["POST"])
 def upload_image():
@@ -10,6 +14,16 @@ def upload_image():
 
     return {"response": "Data Received"}
 
+# TODO Wrap in some request from client to login or sign up
+
+userID = "User1"
+password = "Password1"
+document = {
+    "UserID": userID,
+    "Password": password
+}
+collection.insert_one(document)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    client.close()

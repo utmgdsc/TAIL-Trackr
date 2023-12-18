8a92b6013b5f394cc1f09876693ca795860e9b93
Inaaaam <69984637+inaam10-tech@users.noreply.github.com>
2023-12-17 20:12:30 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import Register from "./components/Register"
 import "./App.css";
 import NewPostPage from "./components/NewPostPage";
 import CreatePost from "./components/CreatePost";
+import Dashboard from "./components/Dashboard";
+
+
 
 
 // app component will control all routes within our program
@@ -24,6 +27,8 @@ function App() {
               <Route path="/new-post" element={<NewPostPage />} />
               {localStorage.getItem("user") ? <Route path="/create-post" element={<CreatePost />} /> : redirect("/")}
               {/* <Route path="/dashboard" element={<VisiblePosts />} /> */}
+              <Route path="/dashboard" element={<Dashboard />} />
+
             </Routes>
         </main>
         </BrowserRouter>
